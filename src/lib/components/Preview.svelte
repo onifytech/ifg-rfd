5acd1db709b653cf38b7a0f1a1bb7130f97e0e03
ammtipparat <amm@onifytech.com>
2025-06-06 16:51:57 +0700

    First commit

---
+++ src/lib/components/Preview.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div class="preview bg-gray-200">
+	<h1>Preview</h1>
+</div>
+
+<style>
+	.preview {
+		width: 100%;
+		padding: 24px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import OutlineLists from '$lib/components/OutlineLists.svelte';
+	import Preview from '$lib/components/Preview.svelte';
+	import Content from '$lib/components/Content.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<OutlineLists />
+	<Content />
+	<Preview />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+		gap: 24px;
+		height: 100vh;
+	}
+</style>
