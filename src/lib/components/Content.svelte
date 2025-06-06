457b821bb4358bfcea1ecdfd27250bb8615126a0
Amm <111744044+ammtipparat@users.noreply.github.com>
2025-06-06 18:18:05 +0700

    First commit (#1)

---
+++ src/lib/components/Content.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+</script>
+
+<div class="content bg-white">
+	<h1>Content</h1>
+	<ul>
+		<li>
+			<h2>RFD1 - Lorem Ipsum</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</li>
+		<li>
+			<h2>RFD2 - Lorem Ipsum</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</li>
+	</ul>
+</div>
+
+<style>
+	.content {
+		width: 50%;
+	}
+	.content h2 {
+		font-size: 24px;
+		font-weight: 600;
+	}
+	.content li {
+		margin-bottom: 28px;
+	}
+</style>
---
+++ src/lib/components/OutlineLists.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<div class="outlineLists bg-gray-200">
+	<h1>Title</h1>
+	<ul>
+		<li><a href="/">List 1</a></li>
+		<li><a href="/">List 2</a></li>
+		<li><a href="/">List 3</a></li>
+	</ul>
+</div>
+
+<style>
+	.outlineLists {
+		width: 25%;
+		padding: 24px;
+	}
+</style>
